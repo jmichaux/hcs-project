8c5639d0142ae1cc686bae73881ab07c27c0c462
hi <nope@nope>
2020-12-03 12:25:50 -0500

    code to slice zonotopes. untested

---
+++ slice_zonotope.m
@@ -0,0 +1,21 @@
+function Z = slice_zonotope(Z, idx, val)
+% SLICE_ZONOTOPE - slize a zonotope given a generator index and value for 
+% its coefficient
+
+% get zonotope as array
+Z = Z.Z;
+c = Z(:,1);
+G = Z(:,2:end);
+
+% get sliceable generator
+g = G(:,idx);
+G(:,idx) = [];
+
+% add generator to center
+c = c + val*g;
+
+% return new zonotope
+Z = [c G];
+Z = zonotope(Z);
+end
+
